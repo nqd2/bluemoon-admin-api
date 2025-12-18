29e5b6641fc4df773f5dc424c33201892c83a560
nqd2 <viet.nh6098@sis.hust.edu.vn>
2025-12-19 03:52:41 +0700

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Bluemoon Admin API up and running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+
