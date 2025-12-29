f09314b87537fc213efc0115579f630edb958546
nqd2 <viet.nh6098@sis.hust.edu.vn>
2025-12-29 13:01:48 +0700

    1

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,8 @@
 import { IUser } from '../models/user.model';
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: IUser;
-    }
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: IUser;
   }
 }
+
